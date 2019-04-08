97c1784fe347f0d21105851d510c8b8b4cc78542
yukha <khartskhaev@gmail.com>
2019-04-08 20:54:20 +0200

    icons

--- demo/DemoWeb/ClientApp/src/app/search-results/search-results.component.ts
+++ demo/DemoWeb/ClientApp/src/app/search-results/search-results.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { SearchSourceType } from '../search-source-type.enum';
+import { ApiServiceService } from '../services/api-service.service';
 import { InitData, InitDataService } from '../services/init-data.service';
 
 @Component({
@@ -13,26 +14,41 @@ import { InitData, InitDataService } from '../services/init-data.service';
 export class SearchResultsComponent {
   private searchedPhrase$ = new BehaviorSubject<string>('');
 
+  searchItems: SearchItem[];
   searchSourceType: SearchSourceType;
   initData: InitData;
 
-  constructor(initDataService: InitDataService, route: ActivatedRoute) {
+  constructor(initDataService: InitDataService, route: ActivatedRoute, api: ApiServiceService) {
     this.searchSourceType = route.snapshot.data.searchSourceType;
     this.initData = initDataService.getInitData(this.searchSourceType);
 
-    this.searchedPhrase$
-      .pipe(
-        debounceTime(300),
-        distinctUntilChanged()
-      )
-      .subscribe(
-        (phrase: string) => console.log('phrase changed: ', phrase),
-        error => console.log(error),
-        () => console.log('complete')
-      );
+    const phrase = this.searchedPhrase$.pipe(
+      debounceTime(300),
+      distinctUntilChanged()
+    );
+
+    this.searchItems = [
+      {
+        searchedPhrase$: phrase,
+        searchSourceType: this.searchSourceType,
+        api,
+      },
+    ];
+
+    // .subscribe(
+    //   (phrase: string) => console.log('phrase changed: ', phrase),
+    //   error => console.log(error),
+    //   () => console.log('complete')
+    // );
   }
 
   search(event) {
     this.searchedPhrase$.next(event.target.value);
   }
+}
+
+export class SearchItem {
+  searchedPhrase$: Observable<string>;
+  searchSourceType: SearchSourceType;
+  api: ApiServiceService;
 }
--- demo/DemoWeb/ClientApp/src/app/toolbar/toolbar.component.ts
+++ demo/DemoWeb/ClientApp/src/app/toolbar/toolbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, HostBinding, OnInit } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-toolbar',
@@ -7,7 +9,12 @@ import { Component, HostBinding, OnInit } from '@angular/core';
 })
 export class ToolbarComponent implements OnInit {
   @HostBinding('class.fixed') fixed = true;
-  constructor() {}
+  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
+    this.matIconRegistry.addSvgIcon(
+      'icon-github',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../../assets/images/github-brands.svg')
+    );
+  }
 
   ngOnInit() {}
 }
